aaac693d6972a9647479f1a45b05c00b4db9e73a
FRANCIS AKA-IGOKWE <francisigbokwe@ukdioninvestment.com>
2021-04-01 08:57:23 +0100

    crud wip

--- blog/database.py
+++ blog/database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
 SQLALCHEMY_DATABASE_URL = 'sqlite:///./blog.db'
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False )
+engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False  })
+
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- blog/main.py
+++ blog/main.py
@@ -1,9 +1,36 @@
-from fastapi import FastAPI
-from . import schemas as sch
+from fastapi import FastAPI, Depends, status, Response
+from . import schemas , models
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
 
 app = FastAPI()
 
+models.Base.metadata.create_all(engine)
 
-@app.post('/blog')
-def create(request : sch.Blog):
-    return request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close
+
+@app.post('/blog', status_code=status.HTTP_201_CREATED)
+def create(request : schemas.Blog, db:Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body = request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+@app.get('/blog')
+def get_posts(db:Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+@app.get('/blog/{id}', status_code=200)
+def show(id, response: Response, db:Session = Depends(get_db)):
+    post = db.query(models.Blog).filter(models.Blog.id == id).first()
+    if(post is not None):
+        return {'data' : post}
+    response.status_code = status.HTTP_404_NOT_FOUND
+    return {'status' : False}
